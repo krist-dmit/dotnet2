c1971191df806429a76cc3b14a093f30ffc999ec
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-01 15:54:01 +0300

    Добавили задачу 6

---
+++ lection/seminar1/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число ");
+
+int Number = Convert.ToInt16(Console.ReadLine());
+
+if (Number mod 2 == 0)
+
+{
+    Console.WriteLine(Number + " - четное число");
+}
+else
+{
+    Console.WriteLine(Number + " - нечетное число");
+}
