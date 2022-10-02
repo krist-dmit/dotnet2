853a6ed72f8271a76dd1ec5c5efeb959076a5cb7
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-02 13:47:29 +0300

    Решили задачу 4

---
+++ homework/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Ввеите первое число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Ввеите второе число: ");
+int b = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Ввеите третье число: ");
+int c = Convert.ToInt16(Console.ReadLine());
+int max = a;
+if (max < b) {
+    max = b;
+           if (max < c) {
+            max = c;
+           }       
+}
+Console.WriteLine("Максимальное число - " + max);
