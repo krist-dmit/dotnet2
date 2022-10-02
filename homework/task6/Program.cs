77f90dd551ac7e12a8384b84d696ad617bf417a9
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-02 13:59:29 +0300

    Решили задачу 6

---
+++ homework/task6/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt16(Console.ReadLine());
+if ( a % 2 == 0) {
+    Console.WriteLine("Число " + a + " четное");
+}
+else {
+    Console.WriteLine("Число " + a + " нечетное");
+}
