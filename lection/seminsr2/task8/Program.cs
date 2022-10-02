3d066274a988fda67b47784c16aac6f6be0c3bfb
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-02 16:31:28 +0300

    Добавили семинар2

---
+++ lection/seminsr2/task8/Program.cs
@@ -0,0 +1,22 @@
+﻿void FindEven(int number)
+{
+    int index = 1;
+    while (index < number)
+    {
+        if (index % 2 == 0)
+        {
+            Console.Write(index + " ");
+        }
+        index++;
+    }
+}
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt16(Console.ReadLine());
+    return number;
+}
+
+int result = GetNumber();
+FindEven(result);
