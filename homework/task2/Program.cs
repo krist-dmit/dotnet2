7b22574631dccf672e5c9bcf9dae4c733bff7e10
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-02 13:28:55 +0300

    Решили задачу 2

---
+++ homework/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt16(Console.ReadLine());
+int max = a;
+int min = b;
+if (a < b) 
+{
+    max = b;
+    min = a;
+}
+
+Console.WriteLine ("Число " + max + " больше, чем " + min);
