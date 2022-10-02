67c01f0e3f03b322bf5090fd8d8bca268258ddbc
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-02 12:57:14 +0300

    Добавили задачу 10

--- lection/seminar1/Task10/Program.cs
+++ lection/seminar1/Task10/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число: ");
+
+int Number = Convert.ToInt16(Console.ReadLine());
+
+int result = Number / 10;
+
+int result1 = result % 10;
+
+Console.WriteLine(result1);
