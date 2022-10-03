fe7feebf381c1bf939dd34c26c11d7b44df2d1b1
vedeneev25 <vedeveev@gmail.com>
2022-10-03 08:02:15 +0300

    Добавил решение 8-ой задачи

---
+++ homework/task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt16(Console.ReadLine());
+int index = 1;
+while (index < number) 
+{
+    if (index % 2 == 0){
+        Console.Write(index + " ");
+    }
+    index ++;
+}
