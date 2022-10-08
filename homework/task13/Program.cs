0687061acc2f10b3a116e0e9db545ff049d803b9
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-08 20:53:44 +0300

    Решили задачу 13

---
+++ homework/task13/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int Vvod()
+{
+    Console.WriteLine("Введите трехзначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int poisk(int chislo)
+    {
+        int result = 0;
+        if (chislo < 100) 
+        {
+            Console.WriteLine("Третьего числа нет");
+        }
+        else {
+            chislo = chislo / 100;
+            result = chislo % 10;
+        }
+        return result;
+    }
+
+int T = Vvod();
+int answer = poisk(T);
+Console.WriteLine(answer);
---
+++ lection/Seminar3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Есть магазин "Ромашка"
+// Охранник ведёт журнал посещения
+// 11-13
+// 10-14
+// 9-10
+// 11-22
+// 18-18
+// 0-23
+// 4-5
+// 4-5
+// 4-5
+
+// Суть: показать интервалы, когда было больше всего 
+// людей в магазине
+
+// Ответ: 4-5, 11-13
+string interval() 
+{
+    string[] interval = new string[9];
+    Console.WriteLine("Введите интервал");
+    int i = 0;
+    while (i<=9)
+    {
+        string interval[i] = Console.ReadLine();
+        i ++;
+    }
+    return interval;
+}
+string[] stringArray = interval;
+Console.Write(interval);
