5f7952ec77a83aeb404d73e91e39d81247770b38
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-08 13:39:48 +0300

    Решили задачу 10 с помощью методов

---
+++ homework/task10/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int Vvod()
+{
+    Console.WriteLine("Введите трехзначное число");
+    int number = Convert.ToInt16(Console.ReadLine());
+    return number;
+}
+
+int Find(int N)
+    {
+     int result = N / 10;
+     int answer = result % 10;
+     return answer;
+    }
+
+int L = Vvod();
+int vtor = Find(L);
+Console.WriteLine(vtor);
