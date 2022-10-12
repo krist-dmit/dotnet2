67cb339742c12702e6994f31e6694460aac4b128
Kristina <kristina@1234s-MacBook-Air.local>
2022-10-12 23:31:09 +0300

    Решили задачу 27

---
+++ homework/task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Vvod (string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int schet (int number)
+{
+    int result = 0;
+    while (number / 10 > 0)
+    {
+       result = result + (number % 10);
+       number = number / 10;
+    }
+    result = result + number;
+    return result;
+}
+Console.WriteLine(schet(Vvod("Введите ваше число: ")));
