a4a98f94115ea70bc3072cf4422fe4e30a5e9b86
vedeneev25 <vedeveev@gmail.com>
2022-10-18 08:05:08 +0300

    задача 36

---
+++ homework/task36/Program.cs
@@ -0,0 +1,27 @@
+﻿Random rnd = new Random();
+
+int[] Array (int count)
+
+{
+    int[] result = new int[count];
+    int i = 0;
+    for (i=0; i < count; i++)
+    {
+        result[i] = rnd.Next(1,3);
+    }
+    return result;
+}
+int SumNechet (int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+    result += array[i];
+    }
+    return result;
+}
+
+int[] array = Array(6);
+
+Console.WriteLine(string.Join(' ',array));
+Console.WriteLine(SumNechet(array));
