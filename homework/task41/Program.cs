34c04cd90b872852eb52e49bbe826e66c4d021fa
vedeneev25 <vedeveev@gmail.com>
2022-10-21 08:06:34 +0300

    Задача 41

---
+++ homework/task41/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] Array (int count, string text)
+{
+    int[] result = new int[count];
+    int i = 0;
+    for (i=0; i < count; i++)
+    {
+        result[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return result;
+}
+int Schet (int[] array)
+{
+    int count = 0; 
+    for (int i = 0; i<array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+int[] massiv = Array(4, "Введите числа");
+Console.WriteLine(string.Join(' ',massiv));
+Console.WriteLine(Schet(massiv));
